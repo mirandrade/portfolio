9702fcb162c6cde65d783450f65623e1a5eb6265
mirandrade <74564032+mirandrade@users.noreply.github.com>
2020-12-01 11:57:55 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,32 @@
+function showPopup1(){
+  document.getElementById("project1-popup").style.display="block";
+  document.getElementById("blackout").style.display="block";
+}
+
+function hidePopup1(){
+  document.getElementById("project1-popup").style.display="none";
+  document.getElementById("blackout").style.display="none";
+
+}
+
+function showPopup2(){
+  document.getElementById("project2-popup").style.display="block";
+  document.getElementById("blackout").style.display="block";
+}
+
+function hidePopup2(){
+  document.getElementById("project2-popup").style.display="none";
+  document.getElementById("blackout").style.display="none";
+
+}
+
+function showPopup3(){
+  document.getElementById("project3-popup").style.display="block";
+  document.getElementById("blackout").style.display="block";
+}
+
+function hidePopup3(){
+  document.getElementById("project3-popup").style.display="none";
+  document.getElementById("blackout").style.display="none";
+
+}
